f26e004614ee9f5e7c1bbabfbf690cd8b08e885e
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-11-09 20:53:54 +0530

    Update gui.py

--- phardwareitk/GUI/gui.py
+++ phardwareitk/GUI/gui.py
@@ -12,7 +12,7 @@ from phardwareitk.ErrorSystem import ErrorSystem as es
 if Unknown_os:
     es.PheonixOsError("Unknown OS detected. Program doesn't know will this GUI system work properly in this OS. To Test, please SET the Windows/Linux/Unix to True and Unknown OS to False in GUI.__init__.")
 
-from phardwareitk.GUI.C import renderGUI
+from phardwareitk.GUI import renderGUI
 
 from OpenGL.GL import *
 import time
