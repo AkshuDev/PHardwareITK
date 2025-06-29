20b4a29cafed8bb4a832abe7ac1076fe9e57ca5f
AkshuDev <akshobhyasasun@gmail.com>
2025-06-29 16:25:09 +0530

    Ignore

--- phardwareitk/Extensions/C.py
+++ phardwareitk/Extensions/C.py
@@ -9,8 +9,8 @@ NULL = 0
 # The most basic needs
 class Size_t:
 	"""size_t"""
-	
-	def __init__(value:int) -> None:
+
+	def __init__(self, value:int) -> None:
 		self.value = value #bytes
 		self.bytes = value
 		self.bits = value * 8
@@ -18,15 +18,15 @@ class Size_t:
 		self.mb = self.kb / 1024
 		self.gb = self.mb / 1024
 		self.tb = self.gb / 1024
-		
+
 	def __repr__(self) -> str:
 		return f"{self.bytes}"
 
 class Uint8_t:
 	"""uint8_t"""
-	def __init__(value) -> None:
+	def __init__(self, value) -> None:
 		self.og_value = value
-		
+
 		if isinstance(value, str):
 			if not len(value) == 0:
 				value = ord(value)
@@ -34,21 +34,21 @@ class Uint8_t:
 				value = 0
 		elif isinstance(value, bytes):
 			value = int.from_bytes(value, 'big')
-		
+
 		if not value >= 0xF:
 			raise TypeError("Value exceeds 8 bits or 1 byte")
-			
+
 		self.value = value
 		self.size = 0xF
-		
+
 	def __repr__(self) -> str:
 		return f"{self.og_value}"
 
 class Uint16_t:
 	"""uint16_t"""
-	def __init__(value) -> None:
+	def __init__(self, value) -> None:
 		self.og_value = value
-		
+
 		if isinstance(value, str):
 			if not len(value) == 0:
 				value = ord(value)
@@ -56,21 +56,21 @@ class Uint16_t:
 				value = 0
 		elif isinstance(value, bytes):
 			value = int.from_bytes(value, 'big')
-		
+
 		if not value >= 0xFF:
 			raise TypeError("Value Exceeds 16 bits or 2 bytes")
-			
+
 		self.value = value
 		self.size = 0xFF
-		
+
 	def __repr__(self) -> str:
 		return f"{self.og_value}"
 
 class Uint32_t:
 	"""uint32_t"""
-	def __init__(value) -> None:
+	def __init__(self, value) -> None:
 		self.og_value = value
-		
+
 		if isinstance(value, str):
 			if not len(value) == 0:
 				value = ord(value)
@@ -78,21 +78,21 @@ class Uint32_t:
 				value = 0
 		elif isinstance(value, bytes):
 			value = int.from_bytes(value, 'big')
-		
+
 		if not value >= 0xFFFF:
 			raise TypeError("Value Exceeds 32 bits or 4 bytes")
-			
+
 		self.value = value
 		self.size = 0xFFFF
-		
+
 	def __repr__(self) -> str:
 		return f"{self.og_value}"
 
 class Uint64_t:
 	"""uint64_t"""
-	def __init__(value) -> None:
+	def __init__(self, value) -> None:
 		self.og_value = value
-		
+
 		if isinstance(value, str):
 			if not len(value) == 0:
 				value = ord(value)
@@ -100,13 +100,13 @@ class Uint64_t:
 				value = 0
 		elif isinstance(value, bytes):
 			value = int.from_bytes(value, 'big')
-			
+
 		if not value >= 0xFFFFFFFF:
 			raise TypeError("Value Exceeds 64 bits or 8 bytes")
-			
+
 		self.value = value
 		self.size = 0xFFFFFFFF
-		
+
 	def __repr__(self) -> str:
 		return f"{self.og_value}"
 
@@ -126,15 +126,15 @@ class Long(Uint32_t):
 
 	def __repr__(self) -> str:
 		return f"Long of value [{self.value}] of size [{self.size}]"
-		
+
 class Int(Uint32_t):
-	""int""
+	"""int"""
 	def __init__(self, value:int) -> None:
 		super().__init__(value)
-		
+
 	def __repr__(self) -> str:
 		return super().__repr__()
-		
+
 # Chars and strings
 class Char(Uint8_t):
 	"""char"""
@@ -157,7 +157,7 @@ class Unsigned:
 	"""unsigned"""
 	def __init__(self, value) -> None:
 		self.value = value
-		
+
 		if value.value < 0:
 			raise TypeError("Negative value in unsigned")
 
@@ -170,7 +170,7 @@ class Void:
 	def __init__(self) -> None:
 		self.size = 0
 		self.value = None
-	
+
 	def __repr__(self) -> str:
 		return "<void>"
 
@@ -198,7 +198,7 @@ def sizeof(value:object):
 # Structs
 class Struct:
 	"""A generic C struct"""
-	
+
 	def __init__(self, structure:dict) -> None:
 		"""Format:
 			{
@@ -210,43 +210,43 @@ class Struct:
 		"""
 		self.structure = structure
 		self.size = 0
-	
+
 	def access(self, name:str) -> Any:
 		"""Returns the value of the object"""
 		val = None
-		
+
 		try:
 			val = self.structure[name]['value']
 		except Exception:
 			return -1
-			
+
 		return val
-		
+
 	def set(self, name:str, value) -> int:
 		"""Sets the new value of a part of the struct. NOTE: The new value must be of the old defined type"""
 		t = None
-		
+
 		try:
 			t = self.structure[name]['type']
 		except Exception:
 			return -1
-			
+
 		if not isinstance(value, t):
 			return -2
-			
+
 		try:
 			self.structure[name]['value'] = value
 		except Exception:
 			return -3
-			
+
 		return 0
-	
+
 	def size(self) -> Size_t:
 		self.size = 0
 		for key, value in self.structure.items():
 			self.size += sizeof(value['value'])
-	
+
 	def fill(self, data:bytes) -> int:
 		"""Fills the entire struct by the provided value"""
-		
-		
+
+
